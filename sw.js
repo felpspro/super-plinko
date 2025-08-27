7cf090ec98a645533a0b35b9928ef82c77ea5804
Vitor Felps <vitor.henrique@felps.com.br>
2025-08-27 17:30:06 -0300

    fix: ajustando pwa

--- sw.js
+++ sw.js
@@ -1,40 +1,46 @@
-const cacheName = "offline-cache-v1";
-const cacheUrls = ["index.html", /* "cat.jpeg" */];
+self.addEventListener('push', event => {
+  const data = event.data ? event.data.json() : {};
 
-// Installing the Service Worker
-self.addEventListener("install", async (event) => {
-  try {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(cacheUrls);
-  } catch (error) {
-    console.error("Service Worker installation failed:", error);
-  }
+  const title = data.title || 'Notificação';
+  const options = {
+    body: data.body || 'Grrupo Felps Professional',
+    // icon: data.icon || '/favicon.png',       // Ícone principal (pequeno)
+    // badge: data.badge || '/badge-icon.png', // Ícone para badge (ex: no Android)
+    image: data.image || undefined,        // Imagem maior dentro da notificação (opcional)
+    vibrate: data.vibrate || [100, 50, 100], // Padrão de vibração (exemplo)
+    actions: data.actions || [],
+    data: {
+      url: data.url || '/' // para usar no clique
+    },
+    requireInteraction: data.requireInteraction || false, // Se true, notificação fica até o usuário interagir
+    silent: data.silent || false // Se true, notificação não faz som
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
 });
 
-// Fetching resources
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(cacheName);
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
 
-      try {
-        const cachedResponse = await cache.match(event.request);
-        if (cachedResponse) {
-          console.log("cachedResponse: ", event.request.url);
-          return cachedResponse;
+  const url = event.notification.data.url;
+  if (event.action === 'open_url') {
+    // Caso usuário clique no botão de ação
+    event.waitUntil(clients.openWindow(url));
+  } else {
+    // Caso clique no corpo da notificação
+    event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+        for (const client of clientList) {
+          if (client.url === url && 'focus' in client) {
+            return client.focus();
+          }
         }
-
-        const fetchResponse = await fetch(event.request);
-        if (fetchResponse) {
-          console.log("fetchResponse: ", event.request.url);
-          await cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
+        if (clients.openWindow) {
+          return clients.openWindow(url);
         }
-      } catch (error) {
-        console.log("Fetch failed: ", error);
-        const cachedResponse = await cache.match("index.html");
-        return cachedResponse;
-      }
-    })()
-  );
+      })
+    );
+  }
 });
